1ed7224507ccfc74f54231612cca5e574728f03b
aidenkhoury <aidenkhoury04@gmail.com>
2025-07-18 17:18:31 +1000

    .

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
-<<<<<<< HEAD
-import topicsReducer from "../features/topics/topicsSlice";
-=======
 import { topicsReducer } from "../features/topics/topicsSlice";
->>>>>>> 96e3c2c7618bc4922fc4189576fb6c070edfcbd7
 
 
 export default configureStore({
